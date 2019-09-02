6d730be1e3eda350223dc5ad730b7e7989c2c096
Robert Begley <rob34@drexel.edu>
2019-09-02 15:55:25 -0400

    Changes

--- app.js
+++ app.js
@@ -107,12 +107,37 @@ app.get("/samples", function(req, res) {
         parts = rows[i].path.split('\\');
         parts = parts.pop();
         //console.log(parts);
-      bigString += "<div><audio controls type='audio/wav' src='../samples/"+ parts +"'></audio></div><div><table class='table'><thead><tr><th scope='col'>Name</th><th scope='col'>Category</th><th scope='col'>Genre</th><th scope='col'>Key</th><th scope='col'>Tempo</th></tr></thead><tbody><tr scope='row'><td>" + rows[i].name+"</td><td>"+rows[i].category+"</td><td>"+rows[i].genre+"</td><td>"+rows[i].musickey+"</td><td>"+rows[i].tempo+"</td></tr></tbody></table></div><br />";
+      bigString += "<div><audio controls type='audio/wav' src='../samples/"+ parts +"'></audio></div><div><table class='table table-dark'><thead><tr><th scope='col'>Name</th><th scope='col'>Category</th><th scope='col'>Genre</th><th scope='col'>Key</th><th scope='col'>Tempo</th></tr></thead><tbody><tr scope='row'><td>" + rows[i].name+"</td><td>"+rows[i].category+"</td><td>"+rows[i].genre+"</td><td>"+rows[i].musickey+"</td><td>"+rows[i].tempo+"</td></tr></tbody></table></div><br />";
     }
       console.log()
       res.send({data: bigString});
   });
 
+});
+
+app.get("/search", function(req, res) {
+  var bigString = "";
+  var name = req.query.name
+  var category = req.query.category
+  var genre = req.query.genre
+  var musickey = req.query.key
+  var tempo = req.query.tempo
+  
+  pool.query("select * from samples ", function(err, rows, fields) {
+      //console.log("rows",rows)
+    if (err) throw err;
+      //console.log("rowslength",rows.length)
+    for (var i = 0; i < rows.length; i++) {
+        //console.log("INFOR")
+        parts = rows[i].path.split('\\');
+        parts = parts.pop();
+        //console.log(parts);
+      bigString += "<div><audio controls type='audio/wav' src='../samples/"+ parts +"'></audio></div><div><table><thead><tr><th>Name</th><th>Category</th><th>Genre</th><th>Key</th><th>Tempo</th></tr></thead><tbody><tr><td>" + rows[i].name+"</td><td>"+rows[i].category+"</td><td>"+rows[i].genre+"</td><td>"+rows[i].musickey+"</td><td>"+rows[i].tempo+"</td></tr></tbody></table></div><br />";
+    }
+      console.log()
+      res.send({data: bigString});
+  });
+  
 });
 
 
